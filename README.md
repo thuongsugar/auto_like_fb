f70b00ffb5eee3f230a232511caaee507b6a6513
XuanThuong01 <phamxuanthuong2301@gmail.com>
2021-10-27 11:10:58 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Auto Like Facebook
+
+## Requirements
+
+-   Install Node<br>
+
+## Config
+
+-   TOKEN=<my token has permissions **pages_manage_engagement**>
+-   USER_ID=<**recipient id**>
+-   LIMIT=<**limit**>
+
+## Development
+
+**`npm i `**</br>
+**`node auto-like.js`**
---
+++ autu_like.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const https = require("https");
+
+function getJsonData(api) {
+    let dataJson = "";
+    return new Promise((t, f) => {
+        https.get(api, (res) => {
+            res.on("data", (data) => {
+                dataJson += data;
+            });
+            res.on("end", () => {
+                t(JSON.parse(dataJson));
+            });
+            res.on("error", (error) => {
+                f(error);
+            });
+        });
+    });
+}
+
+async function getListID(apiListID) {
+    const arrID = await getJsonData(apiListID).then((data) => {
+        const arrData = data.data;
+        const arrID = [];
+        for (let index = 0; index < arrData.length; index++) {
+            arrID.push(arrData[index].id);
+        }
+        return arrID;
+    });
+    console.log("chuan bi" + arrID);
+    return arrID;
+}
+async function startLike(idUser) {
+    const apiGetListId = `https://graph.facebook.com/${idUser}/feed?fields=id&limit=${process.env.LIMIT}&access_token=${process.env.TOKEN}`;
+    let listID = await getListID(apiGetListId);
+    for (let index = 0; index < listID.length; index++) {
+        await postLike(listID[index]);
+    }
+    console.log("Success");
+}
+function postLike(idPost) {
+    const HOST_NAME = "graph.facebook.com";
+    let API_LIKE = `/${idPost}/likes?access_token=${process.env.TOKEN}`;
+    const options = {
+        hostname: HOST_NAME,
+        port: 443,
+        path: API_LIKE,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    };
+    return new Promise((t, f) => {
+        const req = https.request(options, (res) => {
+            console.log(`status code ${res.statusCode}`);
+            res.on("data", (d) => {
+                console.log(JSON.parse(d));
+            });
+            res.on("end", () => {
+                console.log("done " + idPost);
+                t();
+            });
+        });
+        req.on("error", (err) => {
+            console.log(err);
+            f();
+        });
+        req.end();
+    });
+}
+startLike(process.env.USER_ID);
